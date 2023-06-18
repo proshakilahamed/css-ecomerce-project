44d744989b8dd46c251891b4f1d8e59454a42958
Shakil Ahamed <101331131+proshakilahamed@users.noreply.github.com>
2023-06-18 07:20:10 +0600

    Update script.js

--- script.js
+++ script.js
@@ -1,1 +1,33 @@
 
+// JavaScript code for slider functionality
+
+// Get all the slide elements
+const slides = Array.from(document.querySelectorAll('.slide'));
+
+// Get all the control buttons
+const controls = Array.from(document.querySelectorAll('.slider-controls button'));
+
+// Function to show a slide
+function showSlide(index) {
+  // Remove the active class from all slides and control buttons
+  slides.forEach((slide) => {
+    slide.classList.remove('active');
+  });
+  controls.forEach((control) => {
+    control.classList.remove('active');
+  });
+
+  // Add the active class to the selected slide and control button
+  slides[index].classList.add('active');
+  controls[index].classList.add('active');
+}
+
+// Event listener for control button clicks
+controls.forEach((control, index) => {
+  control.addEventListener('click', () => {
+    showSlide(index);
+  });
+});
+
+// Show the initial slide
+showSlide(0);
